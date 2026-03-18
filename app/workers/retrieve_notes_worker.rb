e9a42f1255d3b642a76c4b32bc496b6ab58b4289
GianCalabria <gianluca.calabria@widergy.com>
2026-03-18 12:39:38 -0300

    feat: Implemented index async, fixed things of the first PR

---
+++ app/workers/retrieve_notes_worker.rb
@@ -0,0 +1,20 @@
+class RetrieveNotesWorker < BaseUserWorker
+  private
+
+  def initialize_variables(params)
+    @params = params
+  end
+
+  def perform_args
+    [@params]
+  end
+
+  def service
+    :retrieve_notes
+  end
+
+  def attempt
+    "retrieving notes with params: #{@params}"
+  end
+end
+
